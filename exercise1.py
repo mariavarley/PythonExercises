c4614eb9e6168624c09f4b3fb7e48cb900f55bfd
Maria Varley <mvarley@gmail.com>
2016-08-18 10:21:37 +0100

    first exercise update commit

--- exercise1.py
+++ exercise1.py
@@ -5,14 +5,17 @@
 #(It is true that Python has the max() function built in,
 # but writing it yourself is nevertheless a good exercise.)
 def max(num1, num2):
-        if num1 == num2:
-           return ERROR
-	elif num1 > num2:
+        if num1 > num2:
            return num1
-        else: return num2
+	elif num2 > num1:
+           return num2
+        else: return "Equal"
 
 num1 = input("What's the first number? ")
 num2 = input("What's the second number? ")
 
 largest = max(num1, num2)
-print("The larger number is {0}".format(largest))
+if largest == "Equal":
+   print("The numbers are equal")
+else:
+   print("The larger number is {0}".format(largest))
