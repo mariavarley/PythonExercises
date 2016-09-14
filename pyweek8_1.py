e3096b898a55e61b37ee49989df934bb65877cd4
Maria Varley <mvarley@gmail.com>
2016-09-14 16:29:38 +0100

    Rock, paper, sissors update

--- pyweek8_1.py
+++ pyweek8_1.py
@@ -4,12 +4,8 @@ while True:
    print("Please type rock, paper or sissors to play or quit to quit the game:\n")
    player1 = input("Player1:")
    player2 = input("Player2:")
-   if player1  == "quit":
-      print(player1)
-      print(player2)
-      print("You have chosen to quit the game!")
-      break
-   elif player1 == player2:
+
+   if player1 == player2:
       print("It's a draw!!")
    elif player1 == "rock" and player2 == "scissors":
       print("Player1 is the winner!!")
@@ -19,3 +15,7 @@ while True:
       print("Player1 is the winner!!")
    else:
       print("Player2 is the winner!!")
+
+   play = input("Do you want to play again?\n")
+   if play == "no":
+      break
