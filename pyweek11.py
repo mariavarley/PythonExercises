5781e12878288fabc1f0b3587bca5a59e71c5359
Maria Varley <mvarley@gmail.com>
2016-09-19 19:42:31 +0100

    prime number

--- pyweek11.py
+++ pyweek11.py
@@ -6,15 +6,19 @@ def get_num_from_user():
 def is_num_prime(num):
    a = range(1, num+1)
    b = []
-   for i in a:
-      if num%i == 0:
-         b.append(i)
-
-   if len(b) == 2:
-      print("{0} is a prime number".format(num))
+   if num == 1 or num == 2:
+      print("{0} is not a prime number".format(num))
    else:
-      print("{0} is not a prime number and here are its divisors:{1}".format(num, b)) 
+      for i in a:
+         if num%i == 0:
+            b.append(i)
 
-num = get_num_from_user()
+      if len(b) == 2:
+         print("{0} is a prime number".format(num))
+      else:
+         print("{0} is not a prime number and here are its divisors:{1}".format(num, b)) 
 
-is_num_prime(num)
+while 1:
+   num = get_num_from_user()
+
+   is_num_prime(num)
